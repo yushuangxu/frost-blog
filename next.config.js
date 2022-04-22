a2ab2e5bb20114f939014afa61d047e235cd6627
xiaofei <2953514980@qq.com>
2022-04-22 16:34:39 +0800

    第一次提交

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
 }
 
+module.exports = {
+	webpack: (config, options) => {
+		config.module.rules.push({
+			test: /\.svg$/,
+			include: path.resolve(__dirname, './static/svg'),
+			loader: 'svg-sprite-loader'
+		})
+		return config
+	}
+};
 module.exports = nextConfig
